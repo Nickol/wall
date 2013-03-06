4e39f023f6ab37b5b11ee6da9eb947e2ee1a299d
Nickol <lobanovk@mail.ru>
2013-03-07 00:12:16 +0400

    werwere

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wall::Application.config.secret_token = '750b5e8a8dd765c26477f73da2ec39928812de360b76c647d15aaa8c42342470c6c81f95283744f048d6f4c5a5c6c517b72a1b4a34b6a16fb1c934d818dde97e'
